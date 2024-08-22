0c1117a8c312bd1169a1058dba80482db5c7bedd
Kanthasamy Kowthaman <108736453+kowthaman@users.noreply.github.com>
2024-08-22 15:58:06 +0530

    Update newfile.js

--- newfile.js
+++ newfile.js
@@ -1,4 +1,13 @@
+console.log("hi")
+console.log("done")
+
 console.log("ok")
+
+console.log("hi")
+console.log("done")
+
+console.log("hi")
+console.log("done")
 
 console.log("hi")
 console.log("done")
