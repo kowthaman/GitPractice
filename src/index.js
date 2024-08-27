2b2a96d356feff7d7e56bb1df6678489a6206bdc
Kanthasamy Kowthaman <108736453+kowthaman@users.noreply.github.com>
2024-08-27 08:14:52 +0530

    Update 2

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ const check = async () => {
 console.log("Just Checking ok!!!")
 console.log("Just Checking now for check!!!")
 console.log("Just kidding!!!")
-
+const other = async () => {
+  console.log("other")
+}
 
