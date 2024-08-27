66796ea65150659482a3be00080c0b8a8e22e395
Kowthaman <kandasamykowtham125@gmail.com>
2024-08-27 23:28:25 +0530

    Revert "codes removed that are not necessary"
    This reverts commit f7f2db9ee801f2a814b887f0d07b996c0177ee4b.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 const check = async () => {
   console.log("whether its ok or not")
 }
+const function_ = async () => {
+    console.log("This is my function");
+}
 
+console.log("Just Checking ok!!!")
+console.log("Just Checking now for check!!!")
+console.log("Just kidding!!!")
+const other = async () => {
+  console.log("other")
+}
+const check_2 = async () => {
+  console.log("for only checking");
+}
+console.log("Just Checking ok!!!")
 
 
 
