06e7741ea87e1e94adba23585995eb343898b972
Kowthaman <kandasamykowtham125@gmail.com>
2024-08-27 22:29:27 +0530

    removed files

--- config.js
+++
@@ -1,6 +0,0 @@
-console.log('Hello guys');
-console.log('I am Kowthaman');
-console.log("version 3 is released")
-
-console.log("computer science undergraduate")
-console.log("Trincomalee Campus")
--- newone.js
+++
@@ -1,5 +0,0 @@
-const arr = [];
-console.log("changed");
-console.log("change for check");
-
-const arr2 = [1,2,3,4];
