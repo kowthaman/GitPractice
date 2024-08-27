213b1dcf9f3155a8a62f7e788843fdaa2069f696
Kowthaman <kandasamykowtham125@gmail.com>
2024-08-27 22:32:40 +0530

    added new lines of code

--- change.js
+++ change.js
@@ -1,4 +1,27 @@
 console.log("Change is made now for checking");
+
+app.get(
+    url + "/matri/:id",
+    JwtToken.verify,
+    (req,res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return badResponse(errors.array(), res);
+      }
+      this.serviceCtrl.getMatriById(req, res);
+    }
+  );
+
 console.log("Changes happened lastime");
 
-
+app.put(
+    url + "/matri/:id",
+    JwtToken.verify,
+    (req,res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return badResponse(errors.array(), res);
+      }
+      this.serviceCtrl.deleteMatriPost(req, res);
+    }
+  );
