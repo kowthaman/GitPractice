669d8d29d49add11a8d3ce07b18e547466b7bfc0
Kanthasamy Kowthaman <108736453+kowthaman@users.noreply.github.com>
2024-08-27 22:31:20 +0530

    Update_1

--- change.js
+++ change.js
@@ -1,4 +1,17 @@
+app.post(
+      url + "/surprise/list",
+      Validation.SurprisePackageApiValidation,
+      (req: Request, res: Response) => {
+        this.serviceCtrl.getAllSurprisePackage(req, res);
+      }
+    );
+
 console.log("Change is made now for checking");
 console.log("Changes happened lastime");
 
-
+app.get(
+      url + "/reference",
+      (req: Request, res: Response) => {
+        this.serviceCtrl.getReference(req, res);
+      }
+    );
